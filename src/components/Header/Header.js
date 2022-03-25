a1fa492533d02bca024b2302371fb3156426dc8b
Mahabubul Islam <mahabub2k1@gmail.com>
2022-03-25 22:41:40 +0600

    header and json img added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div>
+            <h1>Chance to win your favourite laptop</h1>
+            <h2>Choose only 4</h2>
+        </div>
+    );
+};
+
+export default Header;
